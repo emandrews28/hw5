e42a11aeeac984c669d8fd94d1946b4ad1e8b1a0
Emma Andrews <emma.f.andrews@gmail.com>
2025-12-04 22:34:47 +0000

    final commit

--- schedwork.cpp
+++ schedwork.cpp
@@ -21,6 +21,8 @@ static const Worker_T INVALID_ID = (unsigned int)-1;
 
 
 // Add prototypes for any helper functions here
+bool schedHelper(int day, int shift, std::vector<int>& workers, const AvailabilityMatrix& avail, const size_t dailyNeed, const size_t maxShifts, DailySchedule& sched);
+bool isValid(int day, int shift, int ID, std::vector<int>& workers, const size_t maxShifts, DailySchedule& sched);
 
 
 // Add your implementation of schedule() and other helper functions here
@@ -37,9 +39,46 @@ bool schedule(
     }
     sched.clear();
     // Add your code below
+    std::vector<int> workers(avail[0].size(), 0);
+    sched.resize(avail.size());
+    for(size_t i=0; i < avail.size(); i++) {
+        for(size_t j=0; j < dailyNeed; j++) {
+            sched[i].push_back(INVALID_ID);
+        }
+    }
+    return schedHelper(0, 0, workers, avail, dailyNeed, maxShifts, sched);
+}
 
+bool schedHelper(int day, int shift, std::vector<int>& workers, const AvailabilityMatrix& avail, const size_t dailyNeed, const size_t maxShifts, DailySchedule& sched) {
+    if(day == sched.size()) {
+      return true;
+    }
+    if(shift == sched[day].size()) {
+      return schedHelper(day+1, 0, workers, avail, dailyNeed, maxShifts, sched);
+    } 
+    if(sched[day][shift] != INVALID_ID) {
+      return schedHelper(day, shift+1, workers, avail, dailyNeed, maxShifts, sched);
+    }
+    for(size_t i=0; i < avail[day].size(); i++) {
+      if(!avail[day][i]) {
+        continue;
+      }
+      sched[day][shift] = i;
+      workers[i]++;
+      if(isValid(day, shift, i, workers, maxShifts, sched)) {
+        if(schedHelper(day, shift+1, workers, avail, dailyNeed, maxShifts, sched)) {
+          return true;
+        }
+      }
+      sched[day][shift] = INVALID_ID;
+    }
+    return false;
+}
 
-
-
+bool isValid(int day, int shift, int ID, std::vector<int>& workers, const size_t maxShifts, DailySchedule& sched) {
+  if(workers[ID] > maxShifts) {
+    return false;
+  }
+  return true;
 }
 
--- wordle.cpp
+++ wordle.cpp
@@ -13,6 +13,9 @@ using namespace std;
 
 
 // Add prototypes of helper functions here
+bool test(const std::string& temp, const std::set<std::string>& dict);
+void placeFloat(int index, std::string& temp, std::string remainder, const std::set<std::string>& dict, std::set<std::string>& words);
+void fill(int index, std::string& temp, const std::set<std::string>& dict, std::set<std::string>& words);
 
 
 // Definition of primary wordle function
@@ -22,7 +25,71 @@ std::set<std::string> wordle(
     const std::set<std::string>& dict)
 {
     // Add your code here
-
+    std::set<std::string> words;
+    std::string temp = in;
+    placeFloat(0, temp, floating, dict, words);
+    return words;
 }
 
 // Define any helper functions here
+void placeFloat(int index, std::string& temp, std::string remainder, const std::set<std::string>& dict, std::set<std::string>& words)
+{
+  if(index == temp.size()) {
+    if(remainder.empty()) {
+      fill(0, temp, dict, words);
+    }
+    return;
+  }
+  if(temp[index] != '-') {
+    placeFloat(index+1, temp, remainder, dict, words);
+    return;
+  }
+
+  int dashes = 0;
+  for(int i=index; i < temp.size(); i++) {
+    if(temp[i] == '-') {
+      dashes++;
+    }
+  }
+  if(remainder.size() > dashes) {
+    return;
+  }
+
+  if(remainder.size() < dashes) {
+    placeFloat(index+1, temp, remainder, dict, words);
+  }
+
+  for(int i=0; i < remainder.size(); i++) {
+    temp[index] = remainder[i];
+    std::string recurseRemain = remainder;
+    recurseRemain.erase(i, 1);
+    placeFloat(index+1, temp, recurseRemain, dict, words);  
+  }
+  temp[index] = '-';
+}
+
+void fill(int index, std::string& temp, const std::set<std::string>& dict, std::set<std::string>& words)
+{
+  if(index == temp.size()) {
+    if(test(temp, dict)) {
+      words.insert(temp);
+    }
+    return;
+  }
+  if(temp[index] != '-') {
+    index++;
+    fill(index, temp, dict, words);
+    return;
+  }
+
+  for(int i=0; i < 26; i++) {
+    temp[index] = 'a' + i;
+    fill(index+1, temp, dict, words);
+  }
+  temp[index] = '-';
+}
+
+
+bool test(const std::string& temp, const std::set<std::string>& dict) {
+  return dict.find(temp) != dict.end();
+}
